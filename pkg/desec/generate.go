ce9e5654ae4635e2eb04164188f3d542e96f8c61
Ethan Li <lietk12@gmail.com>
2022-08-26 19:23:11 -0700

    Use `go generate` for OpenAPI-based code generation (#73)
    * Automatically regenerate zerotier & desec clients with `go generate`
    
    * Fix incorrect configuration names

---
+++ pkg/desec/generate.go
@@ -0,0 +1,5 @@
+package desec
+
+//go:generate oapi-codegen --config types.config.yml desec.yml
+//go:generate oapi-codegen --config client.config.yml desec.yml
+//go:generate oapi-codegen --config server.config.yml desec.yml
---
+++ pkg/zerotier/generate.go
@@ -0,0 +1,5 @@
+package zerotier
+
+//go:generate oapi-codegen --config types.config.yml zerotier.json
+//go:generate oapi-codegen --config client.config.yml zerotier.json
+//go:generate oapi-codegen --config server.config.yml zerotier.json
